e26449dc83fe71763e8822f41101de7a30e63f70
Gratimax <greatdane@ovsankin.com>
2012-07-13 15:51:54 -0700

    Change spells
    - Rename 'oblivion' -> 'explode'
    - Reset explode spell to explode

--- src/net/mdcreator/magica/spell/SpellExplode.java
+++ src/net/mdcreator/magica/spell/SpellExplode.java
@@ -4,11 +4,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.metadata.FixedMetadataValue;
 
-public class SpellOblivion extends Spell{
+public class SpellExplode extends Spell{
 
-    public SpellOblivion(){
-        name = "Oblivion";
-        castName = "Fragrecio";
+    public SpellExplode(){
+        name = "Explode";
+        castName = "recio";
         description = "Explosion, and Lightning intertwined!";
     }
 
@@ -18,10 +18,6 @@ public class SpellOblivion extends Spell{
 
 
     public void cast(Player caster) {
-        for (int j = 0; j < 3; j++)
-    	{
-    		caster.getWorld().strikeLightning(caster.getTargetBlock(null, 512).getLocation());
-    	}
         TNTPrimed tnt = caster.getWorld().spawn(caster.getTargetBlock(null, 512).getLocation(), TNTPrimed.class);
         tnt.setFuseTicks(0);
         tnt.setMetadata("playerSpawned", new FixedMetadataValue(caster.getServer().getPluginManager().getPlugin("Magica"), null));
