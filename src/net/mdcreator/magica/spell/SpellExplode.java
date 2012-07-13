4f77f45d90a6bad2494db4263d1a7ab8ad1b4672
tincopper2 <destroyah321@gmail.com>
2012-07-13 17:16:20 -0500

    Renamed to proper name "Oblivion" and renamed "recio" to "Fragrecio" like we suggested. ;)

--- src/net/mdcreator/magica/spell/SpellExplode.java
+++ src/net/mdcreator/magica/spell/SpellExplode.java
@@ -4,12 +4,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.metadata.FixedMetadataValue;
 
-public class SpellExplode extends Spell{
+public class SpellOblivion extends Spell{
 
-    public SpellExplode(){
-        name = "Explode";
-        castName = "recio";
-        description = "Explodes(only damages) at the pointer";
+    public SpellOblivion(){
+        name = "Oblivion";
+        castName = "Fragrecio";
+        description = "Explosion, and Lightning intertwined!";
     }
 
     public void effects(Player caster) {
@@ -18,6 +18,10 @@ public class SpellExplode extends Spell{
 
 
     public void cast(Player caster) {
+        for (int j = 0; j < 3; j++)
+    	{
+    		caster.getWorld().strikeLightning(caster.getTargetBlock(null, 512).getLocation());
+    	}
         TNTPrimed tnt = caster.getWorld().spawn(caster.getTargetBlock(null, 512).getLocation(), TNTPrimed.class);
         tnt.setFuseTicks(0);
         tnt.setMetadata("playerSpawned", new FixedMetadataValue(caster.getServer().getPluginManager().getPlugin("Magica"), null));
